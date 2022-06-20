9ecd6249af802466af7dd7dc36a994c8df5eaabe
Alex Steele <steeleal123@gmail.com>
2022-06-20 16:47:25 -0700

    Add name service

--- src/ns.lisp
+++ src/ns.lisp
@@ -1,9 +1,21 @@
-;; Name server
+;; Name service
 (defpackage alaman.ns
   (:use #:cl)
-  (:import-from :alaman.core))
+  (:nicknames "ns")
+  (:export #:init
+	   #:register
+	   #:lookup
+	   #:connect))
 
 (in-package :alaman.ns)
 
+(defun init () (make-hash-table :test #'equal))
 
+(defun register (ns name obj &optional data)
+  (setf (gethash name ns) (list obj data)))
 
+(defun connect (ns name)
+  (first (gethash name ns '(nil nil))))
+
+(defun lookup (ns name)
+  (second (gethash name ns '(nil nil))))
