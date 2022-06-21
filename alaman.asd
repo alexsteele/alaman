f7abea7acf5d40b5aa4f8213a2e42fbc688e1dc7
Alex Steele <steeleal123@gmail.com>
2022-06-21 00:18:30 -0700

    wip: Update ns:children

--- alaman.asd
+++ alaman.asd
@@ -1,22 +1,29 @@
-(defsystem "alaman"
+(require :asdf)
+
+(asdf:defsystem "alaman"
   :version "0.1.0"
   :author "Alex Steele"
   :license ""
-  :depends-on ()
+  :depends-on (:alexandria)
   :components ((:module "src"
                 :components
-                ((:file "main"))))
+                ((:file "main")
+		 (:file "core")
+		 (:file "ns")
+		 (:file "agent")
+		 (:file "admin"))))
   :description ""
   :in-order-to ((test-op (test-op "alaman/tests")))
   :entry-point "alaman:main")
 
-(defsystem "alaman/tests"
+(asdf:defsystem "alaman/tests"
   :author ""
   :license ""
   :depends-on ("alaman"
                "rove")
   :components ((:module "tests"
                 :components
-                ((:file "main"))))
+                ((:file "main")
+		 (:file "ns"))))
   :description "Test system for alaman"
   :perform (test-op (op c) (symbol-call :rove :run c)))
--- src/ns.lisp
+++ src/ns.lisp
@@ -1,6 +1,7 @@
 ;; Name service
 (defpackage alaman.ns
   (:use #:cl)
+  (:import-from :alexandria :starts-with-subseq)
   (:nicknames "ns")
   (:export #:init
 	   #:register
@@ -9,6 +10,11 @@
 	   #:children))
 
 (in-package :alaman.ns)
+
+(defstruct entry
+  (name "")
+  (obj nil)
+  (data nil))
 
 (defun init () (make-hash-table :test #'equal))
 
@@ -21,11 +27,31 @@
 (defun lookup (ns name)
   (second (gethash name ns '(nil nil))))
 
-;;; TODO: Add prefix filter
-(defun children (ns name)
-  "Returns a list of '(<name> <obj> <data>)"
-  (let* ((entries nil))
-    (maphash #'(lambda (name entry) (push (append (list name) entry) entries))
+;;; TODO: There must be a better way to do this...
+(defun entries (ns)
+  (let ((result nil))
+    (maphash #'(lambda (name obj-data)
+		 (push (make-entry :name name
+				   :obj (first obj-data)
+				   :data (second obj-data))
+		       result))
 	     ns)
-    entries))
+    result))
 
+(defun children (ns name)
+  "Returns a list of ns:entry"
+  (remove-if #'(lambda (entry)
+		 (print (starts-with-subseq name (entry-name entry)))
+		 (starts-with-subseq name (entry-name entry)))
+	     (entries ns)))
+
+(starts-with-subseq "/dne" "/foo")
+(defun test-ns ()
+  (let ((serv (init)))
+    (register serv "/foo" "a" "b")
+    (assert (equal "b" (lookup serv "/foo")))
+    (assert (equal  "a" (connect serv "/foo")))
+    (assert (equal nil (children serv "/dne")))
+    (assert (equal (list (make-entry :name "/foo" :obj "a" :data "b"))
+		   (children serv "/")))))
+(test-ns)
