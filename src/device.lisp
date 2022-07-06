30bb37a40e880cf042029fff448efe69b2defaac
Alex Steele <steeleal123@gmail.com>
2022-07-05 23:41:22 -0700

    Add battery

--- src/device.lisp
+++ src/device.lisp
@@ -10,10 +10,33 @@
     :initarg :location
     :accessor location)))
 
+(defmethod dostep (device elapsed-sec)
+  "Advance the device state by elapsed-sec."
+  nil)
+
 (defclass thermometer (device) ())
 
 (defmethod read-temp (thermometer) nil)
 
-(defclass battery (device) ())
+(defclass battery (device)
+  ((capacity
+    :initarg :capacity
+    :accessor capacity)
+   (level
+    :initarg :level
+    :accessor level)
+   (drain-rate
+    :initarg :drain-rate ; per second
+    :reader drain-rate)))
+
+(defun new-battery (&key (drain-rate 0))
+  (make-instance 'battery :capacity 100 :level 100 :drain-rate drain-rate))
+
+(defmethod consume (battery n)
+  (assert (>= (level battery) n))
+  (decf (level battery) n))
+
+(defmethod dostep ((b battery) elapsed-sec)
+  (consume b (float (* (drain-rate b) elapsed-sec))))
 
 (defclass solar-panel (device) ())
