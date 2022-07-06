2f6e99846b443b5a3869fce6da5607a8932c3926
Alex Steele <steeleal123@gmail.com>
2022-07-06 00:21:08 -0700

    Fix battery test

--- tests/device.lisp
+++ tests/device.lisp
@@ -14,7 +14,7 @@
   (let ((b (dev:new-battery)))
     (is (= 100 (dev:capacity b)))
     (dev:consume b 50)
-    (is (= 100 (dev:capacity b)))))
+    (is (= 50 (dev:capacity b)))))
 
 (test solar-panel
   nil)
