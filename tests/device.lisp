464cb4ddf70c88188a8db26f34af4f94d0df3369
Alex Steele <steeleal123@gmail.com>
2022-07-05 23:56:38 -0700

    Add battery test

---
+++ tests/device.lisp
@@ -0,0 +1,22 @@
+(defpackage alaman/tests/device
+  (:use :cl :alaman :fiveam :alaman.core)
+  (:import-from :alaman.device)
+  (:local-nicknames
+   (:dev :alaman.device)))
+(in-package :alaman/tests/device)
+
+(def-suite* device-tests
+  :description "device tests")
+
+(in-suite device-tests)
+
+(test battery
+  (let ((b (dev:new-battery)))
+    (is (= 100 (dev:capacity b)))
+    (dev:consume b 50)
+    (is (= 100 (dev:capacity b)))))
+
+(test solar-panel
+  nil)
+
+(run! 'device-tests)
