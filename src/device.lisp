87d14027692eb9d3baa5fa95739d852361c27d31
Alex Steele <steeleal123@gmail.com>
2022-07-07 00:09:41 -0700

    Add solar-panel

--- src/device.lisp
+++ src/device.lisp
@@ -1,5 +1,10 @@
 (defpackage alaman.device
-  (:use #:cl))
+  (:use #:cl)
+  (:import-from :alaman.core)
+  (:import-from :alexandria :when-let)
+  (:local-nicknames
+   (:core :alaman.core)))
+;; Note: export-all below
 (in-package :alaman.device)
 
 (defclass device ()
@@ -32,6 +37,10 @@
 (defun new-battery (&key (drain-rate 0))
   (make-instance 'battery :capacity 100 :level 100 :drain-rate drain-rate))
 
+(defmethod replenish (battery n)
+  (setf (level battery) (min (+ n (level battery))
+			     (capacity battery))))
+
 (defmethod consume (battery n)
   (assert (>= (level battery) n))
   (decf (level battery) n))
@@ -39,4 +48,40 @@
 (defmethod dostep ((b battery) elapsed-sec)
   (consume b (float (* (drain-rate b) elapsed-sec))))
 
-(defclass solar-panel (device) ())
+(defclass solar-panel (device)
+  ((battery
+    :initarg :battery
+    :accessor battery)
+   (universe
+    :initarg :universe
+    :reader universe)
+   (fill-rate
+    :initarg :fill-rate
+    :reader fill-rate)))
+
+(defun new-solar-panel (&key location universe (fill-rate 1.0) (battery nil))
+  (assert location)
+  (assert universe)
+  (make-instance 'solar-panel :location location
+			      :universe universe
+			      :fill-rate fill-rate
+			      :battery battery))
+
+(defmethod attach ((panel solar-panel) battery)
+  (setf (battery panel) battery))
+
+(defmethod detach ((panel solar-panel))
+  (setf (battery panel) nil))
+
+(defmethod sunnyp ((panel solar-panel))
+  (let* ((uni (universe panel))
+	 (loc (location panel))
+	 (tile (core:tile-at-loc uni loc))
+	 (climate (core:tile-climate tile)))
+    (equalp :sunny climate)))
+
+(defmethod dostep ((panel solar-panel) elapsed-sec)
+  (when (and (battery panel) (sunnyp panel))
+    (replenish (battery panel) (float (* (fill-rate panel) elapsed-sec)))))
+
+(core:export-all :alaman.device)
--- tests/device.lisp
+++ tests/device.lisp
@@ -1,8 +1,10 @@
 (defpackage alaman/tests/device
   (:use :cl :alaman :fiveam :alaman.core)
   (:import-from :alaman.device)
+  (:import-from :alaman.map)
   (:local-nicknames
-   (:dev :alaman.device)))
+   (:dev :alaman.device)
+   (:am :alaman.map)))
 (in-package :alaman/tests/device)
 
 (def-suite* device-tests
@@ -15,9 +17,33 @@
     (is (= 100 (dev:capacity b)))
     (is (= 100 (dev:level b)))
     (dev:consume b 60)
-    (is (= 40 (dev:level b)))))
+    (is (= 40 (dev:level b)))
+'    (dev:replenish b 100) ; fills to capacity=100
+    (is (= 100 (dev:level b)))))
 
 (test solar-panel
-  nil)
+  (let* ((tiles (am:uniform-map '(1 1) :climate :sunny))
+	 (U (make-universe :tiles tiles))
+	 (panel (dev:new-solar-panel :location '(0 0)
+				     :universe U
+				     :fill-rate 1.0))
+	 (bat (new-battery)))
+
+    (is (null (dev:battery panel)))
+    (dev:detach panel) ; no-op
+
+    (dev:dostep panel 10)
+
+    (dev:attach panel bat)
+    (dev:consume bat 50)
+
+    ; sunny? replenish battery
+    (dev:dostep panel 10)
+    (is (equalp 60.0 (dev:level bat)))
+
+    ; cloudy? no change
+    (fill-tiles tiles :climate :cloudy)
+    (dev:dostep panel 10)
+    (is (equalp 60.0 (dev:level bat)))))
 
 (run! 'device-tests)
