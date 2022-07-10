a3e32c17b395b54aa46ef5b4b6f53f3904fd8dc8
Alex Steele <steeleal123@gmail.com>
2022-07-10 15:50:30 -0700

    Update command params to alist

--- src/command.lisp
+++ src/command.lisp
@@ -1,7 +1,9 @@
 (defpackage alaman.command
   (:use #:cl #:alaman.core)
-  (:export #:move-to
-	   #:sleep-until))
+  (:export #:no-op
+	   #:move-to
+	   #:sleep-until
+	   #:param))
 (in-package :alaman.command)
 
 (defun no-op ()
@@ -13,10 +15,13 @@
   (make-command :id (new-id)
 		:kind :move
 		:state :start
-		:params (list :location location)))
+		:params (list (cons :location location))))
 
 (defun sleep-until (until)
   (make-command :id (new-id)
 		:kind :sleep
 		:state :start
-		:params (list :until until)))
+		:params (list (cons :until until))))
+
+(defun param (cmd name)
+  (cdr (assoc name (command-params cmd))))
