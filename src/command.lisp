c7ae2cc2ec92d3ef8ab6d25cb819fede1b1d4836
Alex Steele <steeleal123@gmail.com>
2022-07-20 00:44:29 -0700

    Add explore command

--- src/command.lisp
+++ src/command.lisp
@@ -25,6 +25,13 @@
 		:state :new
 		:params (list (cons :until until))))
 
+(defun explore (&optional bounds &key (end-time nil))
+  "bounds: bounding rectangle '((x1 y1) (x2 y2))"
+  (make-command :id (new-id)
+		:kind :explore
+		:state :new
+		:params (list (cons :bounds bounds) (cons :end-time end-time))))
+
 (defun state (cmd)
   (command-state cmd))
 
