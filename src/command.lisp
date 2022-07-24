f1799343071430b527dec7302749edf4a0274eba
Alex Steele <steeleal123@gmail.com>
2022-07-24 00:01:31 -0700

    cmd: use hash-table for params

--- src/command.lisp
+++ src/command.lisp
@@ -1,5 +1,6 @@
 (defpackage alaman.command
   (:use #:cl #:alaman.core)
+  (:import-from #:serapeum :dict :href)
   (:export #:no-op
 	   #:move-to
 	   #:sleep-until
@@ -19,33 +20,34 @@
   (make-command :id (new-id)
 		:kind :move
 		:state :new
-		:params (list (cons :location location))))
+		:params (dict :location location)))
 
 (defun sleep-until (until)
   (make-command :id (new-id)
 		:kind :sleep
 		:state :new
-		:params (list (cons :until until))))
+		:params (dict :until until)))
 
 (defun explore (&optional bounds &key (end-time nil))
   "bounds: bounding rectangle '((x1 y1) (x2 y2))"
   (make-command :id (new-id)
 		:kind :explore
 		:state :new
-		:params (list (cons :bounds bounds) (cons :end-time end-time))))
+		:params (dict cons :bounds bounds :end-time end-time)))
 
 (defun gather (quantity resource &key (end-time nil))
   (make-command :id (new-id)
 		:kind :explore
 		:state :new
-		:params (list (cons :quantity quantity) (cons :resource resource)
-			      (cons :end-time end-time))))
+		:params (dict :quantity quantity
+			      :resource resource
+			      :end-time end-time)))
 
 (defun state (cmd)
   (cmd-state cmd))
 
 (defun param (cmd name)
-  (cdr (assoc name (cmd-params cmd))))
+  (href (cmd-params cmd) name))
 
 (defun cvar (cmd name)
   (cdr (assoc name (cmd-vars cmd))))
